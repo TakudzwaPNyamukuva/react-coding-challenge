37a8aba22158784edc37fd46b5620af9266a2b58
Takudzwa Nyamukuva <takudzwanyamukuva@gmail.com>
2022-10-18 21:08:30 +0200

    ready for testing

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,4 +1,5 @@
 import { IAppContext, ICardsData } from '@/interfaces'
+import { cardsData } from '@/mockData'
 import React, { createContext, useState } from 'react'
 
 interface IAppProvider {
@@ -8,30 +9,7 @@ interface IAppProvider {
 const AppContext = createContext<IAppContext | undefined>(undefined)
 
 export function AppProvider({ children }: IAppProvider) {
-	const [data, setData] = useState<ICardsData[] | undefined>([
-		{ id: 1, page: 1, bg: 'bg-blue-600', color: 'text-blue-100', name: 'item 1', pinned: false },
-		{ id: 2, page: 1, bg: 'bg-red-600', color: 'text-red-100', name: 'item 2', pinned: false },
-		{
-			id: 3,
-			page: 1,
-			bg: 'bg-purple-600',
-			color: 'text-purple-100',
-			name: 'item 3',
-			pinned: false,
-		},
-		{ id: 4, page: 1, bg: 'bg-green-600', color: 'text-green-100', name: 'item 4', pinned: false },
-		{ id: 5, page: 2, bg: 'bg-amber-600', color: 'text-amber-100', name: 'item 1', pinned: false },
-		{ id: 6, page: 2, bg: 'bg-lime-600', color: 'text-lime-100', name: 'item 2', pinned: false },
-		{
-			id: 7,
-			page: 2,
-			bg: 'bg-teal-600',
-			color: 'text-teal-100',
-			name: 'item 3',
-			pinned: false,
-		},
-		{ id: 8, page: 2, bg: 'bg-rose-600', color: 'text-rose-100', name: 'item 4', pinned: false },
-	])
+	const [data, setData] = useState<ICardsData[] | undefined>(cardsData)
 
 	const contextData = {
 		data,
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,14 +7,6 @@ export interface ICardsData {
 	pinned: boolean
 }
 
-export interface IPinnedData {
-	id: number
-	page: number
-	bg: string
-	color: string
-	name: string
-}
-
 export interface IAppContext {
 	data: ICardsData[] | undefined
 	setData: React.Dispatch<React.SetStateAction<ICardsData[] | undefined>>
